c04597fbc0e96628de731c0a1f9b209689b85e67
Dan Hahn <daniel.hahn@michaelkors.com>
2018-06-05 23:01:26 -0400

    removeing old site

--- js/homepage.js
+++
@@ -1,4 +0,0 @@
-const lessons = document.querySelectorAll('.lesson');
-const date = new Date();
-const today = `${date.getMonth()+1}${date.getDate()}${date.getFullYear()}`
-lessons.forEach(lesson => lesson.classList.contains(today) ? lesson.classList.add('active') : null);
