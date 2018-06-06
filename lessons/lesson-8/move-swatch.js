04f823feab8e22977897feefdaa468d7cb02aa8d
Dan Hahn <daniel.hahn@michaelkors.com>
2018-06-05 23:09:11 -0400

    Revert files

--- lessons/lesson-8/move-swatch.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('ul[id*=slot]').forEach((swatch,i) => {
-  document.getElementById(`slot${i+1}`).innerHTML = swatch.outerHTML;
-  swatch.remove();
-})
-
-document.querySelectorAll('#mainSwatches').forEach((swatch,i) => {
-  document.getElementById('swatches').innerHTML = swatch.outerHTML;
-  swatch.remove();
-})
